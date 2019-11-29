279050b354849a3d9c7aa7e10596732b0feb78ee
ramdhanihendrie <hendrieramdhani@gmail.com>
2019-11-29 15:06:23 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pedigree</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+
+<body>
+	<div id="container">
+		<div id="header">
+			<h1 style="flex-grow: 1">Pedigree</h1>
+			<div style="flex-grow: 2" id="navmenu">
+				<a href="index.php">Home</a>
+				<a href="about.php">About</a>
+				<a href="admin.php">Admin</a>
+			</div>
+			<div id="butmen" style="flex-grow: 2" align="right">
+				<button>abc</button>
+				<button>asd</button>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>

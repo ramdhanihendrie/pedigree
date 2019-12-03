292445476fb75167e34fff180e7304ee6289b665
ramdhanihendrie <hendrieramdhani@gmail.com>
2019-12-03 20:03:58 +0700

    perbaikan UI

---
+++ content/home.php
@@ -0,0 +1,1 @@
+<h1>Welcome To Pedigree</h1>
--- index.php
+++ index.php
@@ -7,17 +7,30 @@
 
 <body>
 	<div id="container">
-		<div id="header">
-			<h1 style="flex-grow: 1">Pedigree</h1>
-			<div style="flex-grow: 2" id="navmenu">
-				<a href="index.php">Home</a>
-				<a href="about.php">About</a>
-				<a href="admin.php">Admin</a>
+		<div id="background">
+			<div id="header">
+				<h1 style="flex-grow: 1">Pedigree</h1>
+				<div style="flex-grow: 2" id="navmenu">
+					<a href="?module=home#pos">Home</a>
+					<a href="?module=about#pos">About</a>
+					<a href="?module=admin#pos">Admin</a>
+				</div>
+				<div id="butmen" style="flex-grow: 2" align="right">
+					<input type="submit" name="signin" value="Sign-In" id="navsignin">
+					<input type="submit" name="signup" value="Sign-Up" id="navsignup">
+				</div>
 			</div>
-			<div id="butmen" style="flex-grow: 2" align="right">
-				<button>abc</button>
-				<button>asd</button>
+
+			<div id="page">
+				<?php if(isset ($_GET ['module']))
+					include "content/$_GET[module].php";
+				else
+					include "content/home.php";?>
 			</div>
+		</div>
+
+		<div id="footer">
+			<h1>2019</h1>
 		</div>
 	</div>
 
